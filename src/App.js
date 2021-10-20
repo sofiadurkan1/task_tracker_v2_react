d8674eaa97238e1c039ced3c062bc5b57164b20c
Sofia <sofiadurkan@gmail.com>
2021-10-19 23:15:11 -0500

    ondelete on toggle added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState()
+
+  //Delete task  
+
+  const deleteTask =(id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder task
+
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
+
   return (
     <div className="container">
      <Header/>
-     <Tasks/>
+     {tasks.length >0 ? (<Tasks  onDelete={deleteTask} onToggle={toggleReminder}/>) : ( 'No Task To Show' ) }
 
      
     </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task }) => {
+const Task = ({ task,onDelete,onToggle}) => {
     return (
-        <div className='task'>
-            <h3>{task.text}<FaTimes style={{color:'red', cursor:'pointer' }}/></h3>
+        <div className='task' onDoubleClick={() =>onToggle(task.id)}>
+            <h3>{task.text}<FaTimes onClick={() =>onDelete(task.id)} style={{color:'red', cursor:'pointer' }}/></h3>
             <p>{task.day}</p>
             
         </div>
